af5c39bd4cb6ffb0ce2cc17c1ca5bf82f76b5b58
Cara Lill <caralill1331@gmail.com>
2025-06-08 22:21:18 +1200

    Setting up height and aspect ratio lock foundations.

--- frontend/src/views/ConverterView.vue
+++ frontend/src/views/ConverterView.vue
@@ -1,6 +1,6 @@
 <template>
 	<h1>Convert Here</h1>
-	<input type="file" @change="handleImgChange" />
+	<input style="color: white" type="file" @change="handleImgChange" />
 
 	<br> <br>
 	<div class="settings">
@@ -12,8 +12,11 @@
 		<label for="contrast">Contrast: </label>
 		<input type="range" id="contrast" v-model.number="contrast" min="0.1" max="3" step="0.1">
 		<span>{{ contrast }}</span>
+		<br>
 		<label for="width">Width (10-500):</label>
 		<input type="number" id="width" v-model.number="width" min="10" max="500">
+		<label for="height">Height (10-500) *not working:</label>
+		<input type="number" id="height" v-model.number="height" min="10" max="500">
 	</div> 
 	<br>
 
@@ -33,6 +36,10 @@ const isColour = ref(false);
 const brightness = ref(1);
 const contrast = ref(1);
 const width = ref(200);
+const height = ref(100);
+const isLandscape = ref(true);
+const aspectRatio = ref(0);
+const aspectLock = ref(true);
 const fontSize = ref(10);
 const asciiArtHtml = ref<string>("");
 
@@ -40,6 +47,20 @@ const handleImgChange = (event: Event) => {
 	const files = (event.target as HTMLInputElement).files;
 	if (files && files.length > 0) {
 		uploadedImg.value = files[0];
+
+		// Get aspect ratio and whether it is landscape
+		const img = new Image();
+		const objectUrl = URL.createObjectURL(files[0]);
+		img.src = objectUrl;
+		img.onload = () => {
+			aspectRatio.value = img.naturalWidth/img.naturalHeight;
+			isLandscape.value = img.naturalWidth >= img.naturalHeight
+			console.log(`Image dimensions: ${img.naturalWidth}x${img.naturalHeight}`);
+			URL.revokeObjectURL(objectUrl);
+		};
+			img.onerror = () => {
+			URL.revokeObjectURL(objectUrl);
+		};
 	}
 };
 
@@ -52,8 +73,8 @@ const submitImgToConverter = async () => {
 	formData.append('image', uploadedImg.value);
 	formData.append('config', JSON.stringify({ output_width: width.value, brightness_factor: brightness.value, contrast_factor: contrast.value, is_color: isColour.value }))
 	
-	//const response = await fetch('https://192.168.68.59:8444/convert-image', {
-	const response = await fetch('https://192.168.1.95:8444/convert-image', {
+	const response = await fetch('https://192.168.68.59:8444/convert-image', {
+	//const response = await fetch('https://192.168.1.95:8444/convert-image', {
 		method: 'POST',
 		body: formData,
 	});
