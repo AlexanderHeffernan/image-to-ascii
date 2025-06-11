d5a8a012641ad059ee9d4a218b292fe3d0f0a26f
Cara Lill <caralill1331@gmail.com>
2025-06-12 11:19:22 +1200

    Added three different character sets that the user can switch between.

--- frontend/src/views/ConverterView.vue
+++ frontend/src/views/ConverterView.vue
@@ -4,6 +4,13 @@
 
 	<br> <br>
 	<div class="settings">
+		<label for="charSet">Character Set: </label>
+		<select id="charSet" v-model="charSet">
+			<option value="standard">Standard</option>
+			<option value="simple">Simple</option>
+			<option value="complex">Complex</option>
+		</select>
+		<br>
 		<label for="colourCheck">Colour: </label>
 		<input type="checkbox" id="colourCheck" v-model="isColour">
 		<label for="brightness">Brightness: </label>
@@ -39,6 +46,7 @@
 import { ref } from 'vue';
 
 const uploadedImg = ref<File|null>(null);
+const charSet = ref("standard");
 const isColour = ref(false);
 const brightness = ref(1);
 const contrast = ref(1);
@@ -92,9 +100,31 @@ const submitImgToConverter = async () => {
 		alert("Please first upload an image.");
 	}
 
+	let chars = "";
+
+	if (charSet.value === "standard") {
+		chars = " .:-=+*#%@";
+	}
+	else if (charSet.value === "simple") {
+		chars = " .-*#@"
+	}
+	else if (charSet.value === "complex") {
+		chars = " .ʼ`^,:;Iil!i><~+_-?][}{1)(|/tfrjxnuvcxzYXUJCLQ0OZmwpqbdkhao*#MW&8%B@$"
+	}
+
+
+
 	const formData = new FormData();
 	formData.append('image', uploadedImg.value);
-	formData.append('config', JSON.stringify({ output_width: width.value, output_height: height.value, brightness_factor: brightness.value, contrast_factor: contrast.value, is_color: isColour.value, aspect_ratio_correction: aspectRatioCorrection.value }))
+	formData.append('config', JSON.stringify({ 
+		character_set: chars.split(''),
+		output_width: width.value, 
+		output_height: height.value, 
+		brightness_factor: brightness.value, 
+		contrast_factor: contrast.value, 
+		is_color: isColour.value, 
+		aspect_ratio_correction: aspectRatioCorrection.value 
+	}));
 	
 	//const response = await fetch('https://192.168.68.59:8444/convert-image', {
 	const response = await fetch('https://192.168.1.95:8444/convert-image', {
@@ -123,6 +153,12 @@ button {
 	color: black;
 }
 input {
+	color: black;
+}
+select {
+	color: black;
+}
+option {
 	color: black;
 }
 </style>
